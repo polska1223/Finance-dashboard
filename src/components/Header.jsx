4a105e066efb03507b56db024db6999dbb651563
MD-DD <Mikolajdanicki34@gmail.com>
2026-03-17 10:37:26 +0100

    Eerste commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header>
+      <h1>Finance Dashboard</h1>
+      <p>Overzicht van crypto coins</p>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h2>Welkom op mijn dashboard</h2>
+      <p>Hier komt straks de lijst met coins te staan.</p>
+    </div>
+  );
+}
+
+export default Home;
